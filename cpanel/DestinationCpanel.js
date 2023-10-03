eb4cf148a2899caaf29061f48fb07344e08af7bc
AiCyZy <tranhet79@gmail.com>
2023-10-03 20:26:51 +0700

    Update DestinationCpanel.js

--- cpanel/DestinationCpanel.js
+++ cpanel/DestinationCpanel.js
@@ -9,7 +9,7 @@ const multer = require("multer");
 const appFirebase = require("../component/config/FirebaseConfig")
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage();
-const storageRef = ref(storage, 'tour/');
+const storageRef = ref(storage, 'destination/');
 const multerStorage = multer.memoryStorage();
 const uploadImage = multer({ storage: multerStorage });
 

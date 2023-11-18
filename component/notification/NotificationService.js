61ab78ad2f5077abd11463c5cd1973baddc9bb06
tuananh2k321 <haizzj123@gmail.com>
2023-11-18 20:56:17 +0700

    TA update code

--- component/notification/NotificationService.js
+++ component/notification/NotificationService.js
@@ -40,4 +40,29 @@ const addNotification = async (image, title, content, timestamp, type, user_id, 
   return false;
 };
 
-module.exports = { getNotificationByIdUser, addNotification };
+const addNotificationNewTour = async (image, title, content, timestamp, type, user_id, tour_id) => {
+  try {
+    const newNotification = {
+      image,
+      title,
+      content,
+      timestamp,
+      type,
+      user_id,
+      tour_id
+    };
+    const notification = new NotificationModel(newNotification)
+    const savedNotification = await notification.save();
+    console.log("service savedNotification: ",savedNotification)
+    if (savedNotification) {
+        return savedNotification;
+    } else {
+        return false
+    }
+  } catch (error) {
+    console.log("gert list err: ", error);
+  }
+  return false;
+};
+
+module.exports = { getNotificationByIdUser, addNotification, addNotificationNewTour };
--- routes/api/NotificationApi.js
+++ routes/api/NotificationApi.js
@@ -49,6 +49,46 @@ router.get("/push-notification-feedback", async (req, res, next) => {
   }
 });
 
+//http://localhost:3000/notification/api/push-notification-new-tour?registrationToken=""&userId=""
+router.get("/push-notification-new-tour", async (req, res, next) => {
+  try {
+    const { registrationToken, userId, tourId } = req.query;
+
+    const currentTime = new Date().toLocaleTimeString();
+    const message = {
+      notification: {
+        title: "Phản hồi của khách hàng!",
+        body: "Hãy ghi ra những trải nghiệm trong chặng hành trình của bạn!",
+      },
+      data: {
+        score: "850",
+        time: currentTime,
+      },
+      token: registrationToken,
+    };
+
+    const image = 'https://firebasestorage.googleapis.com/v0/b/travelapp-3e538.appspot.com/o/user-avatar%2Flogo.png?alt=media&token=94c7da08-1361-4b42-9f7c-b993c03b85f1'
+    const title = "Phản hồi của khách hàng!"
+    const content = "Hãy ghi ra những trải nghiệm trong chặng hành trình của bạn!"
+    const timeStamp = currentTime
+    const type = "new-tour"
+    const notification = await notificationService.addNotificationNewTour(image, title, content, timeStamp,type, userId, tourId)
+    console.log(notification)
+    if (notification) {
+      const response = await admin.messaging().send(message);
+      console.log("Successfully sent message:", response);
+      res.status(200).json({ result: true, notification: notification, message: "success" });
+    } else {
+      res.status(400).json({ result: true, notification: null, message: "fail" });
+    }
+    
+    
+  } catch (error) {
+    console.log("Error sending message:", error);
+    res.status(400).json({ result: false, error: error, message: "fail" });
+  }
+});
+
 //http://localhost:3000/notification/api/getNotification?userId=""
 router.get("/getNotification", async (req, res, next) => {
   try {

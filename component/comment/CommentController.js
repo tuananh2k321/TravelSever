ed3aedbe62a483dba2ad4c99f3e06adf29e3b633
tuananh2k321 <haizzj123@gmail.com>
2023-11-19 22:14:21 +0700

    TA update comment API

--- component/comment/CommentController.js
+++ component/comment/CommentController.js
@@ -18,6 +18,15 @@ const getAllCommentOfTour = async ( tour_id) => {
     }
 }
 
+const getTopCommentOfTour = async ( tour_id) => {
+    try {
+        return await commentService.getTopCommentOfTour( tour_id);
+
+    } catch (error) {
+        return false;
+    }
+}
+
 module.exports = {
-    addComment, getAllCommentOfTour
+    addComment, getAllCommentOfTour, getTopCommentOfTour
 };
--- routes/api/CommentApi.js
+++ routes/api/CommentApi.js
@@ -41,4 +41,20 @@ router.get("/listComment", async (req, res, next) => {
     }
   });
 
+//http://localhost:3000/comment/api/topListComment?tour_id=
+router.get("/topListComment", async (req, res, next) => {
+  try {
+  const {tour_id} = req.query
+    const comments = await commentController.getTopCommentOfTour(tour_id);
+    const numberOfComments = comments.length;
+    const ratings = comments.map(comment => comment.rating);
+
+    // Tính trung bình các giá trị rating (nếu cần)
+    const averageRating = ratings.length > 0 ? ratings.reduce((a, b) => a + b) / ratings.length : 0;
+    return res.status(200).json({ result: true, comments: comments, quantity: numberOfComments, averageRating: averageRating });
+  } catch (error) {
+    console.log("listComment:" + error);
+  }
+});
+
 module.exports = router;

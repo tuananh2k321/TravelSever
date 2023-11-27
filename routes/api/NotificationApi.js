a467f36d354c59fe0cc8e0c12555ab2d1f61781b
tuananh2k321 <haizzj123@gmail.com>
2023-11-27 20:13:52 +0700

    Ta update code

--- routes/api/NotificationApi.js
+++ routes/api/NotificationApi.js
@@ -98,6 +98,50 @@ router.get("/push-notification-confirm", async (req, res, next) => {
   }
 });
 
+//http://localhost:3000/notification/api/push-notification-confirm?userId=""&tourId=""&id=""
+router.get("/push-notification-delete", async (req, res, next) => {
+  try {
+    const { userId, tourId, id } = req.query;
+    const tokens = await tokenController.getTokenByUserId(userId);
+    const tokensArray = tokens.map((tokenObj) => tokenObj.token);
+    console.log(tokensArray);
+
+    const currentTime = new Date().toLocaleTimeString();
+    const message = {
+      notification: {
+        title: "Hủy tour thành công!",
+        body: "Hãy sớm đặt lại tour nhé!",
+      },
+      data: {
+        score: "850",
+        time: currentTime,
+      },
+      tokens: tokensArray,
+    };
+
+    const image = 'https://firebasestorage.googleapis.com/v0/b/travelapp-3e538.appspot.com/o/user-avatar%2Flogo.png?alt=media&token=94c7da08-1361-4b42-9f7c-b993c03b85f1'
+    const title = "Hủy tour thành công!"
+    const content = "Hãy sớm đặt lại tour nhé!"
+    const timeStamp = currentTime
+    const type = "delete"
+    const notification = await notificationService.addNotification(image, title, content, timeStamp,type, userId, tourId)
+    await bookingService.deleteBooking(id)
+    console.log(notification)
+    if (notification) {
+      const response = await admin.messaging().sendEachForMulticast(message);
+      //console.log("Successfully sent message:", response);
+      res.status(200).json({ result: true, notification: notification, message: "success" });
+    } else {
+      res.status(400).json({ result: true, notification: null, message: "fail" });
+    }
+    
+    
+  } catch (error) {
+    console.log("Error sending message:", error);
+    res.status(400).json({ result: false, error: error, message: "fail" });
+  }
+});
+
 
 
 
